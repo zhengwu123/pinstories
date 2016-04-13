c8a8be56a59d761ceb9daa82453eaa3f8b3b39f8
wu <wu673@purdue.edu>
2016-04-13 00:04:20 -0400

    format

--- index.php
+++ index.php
@@ -13,12 +13,11 @@
 	
 		<form action="process_form.php" method="post">
     
-<input type="text" name="username" placeholder="Enter Username">
-<input type="password"  name="password" placeholder="Enter Password"><br>
-<input type="submit" name="submit">
+		<input type="text" name="username" placeholder="Enter Username">
+		<input type="password"  name="password" placeholder="Enter Password"><br>
+		<input type="submit" name="submit">
     
-    
-</form>
+    	</form>
 	</div>
 	
 	<ul class="bg-bubbles">
@@ -35,6 +34,5 @@
 	</ul>
 </div>
 
-</script>
 </body>
 </html>
