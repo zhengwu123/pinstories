7469f2203a4dc27d2f1d8dbda5bdda6d8acd332e
zheng wu <wu673@purdue.edu>
2016-04-12 23:59:15 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,4 @@
 <!DOCTYPE html>
-//zheng add php file here
-<?php 
-
-if(isset($_POST['submit'])) {
-    echo "hello";
-    
-    $minimun = 5;
-    $maximun = 30;
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-    
-  if(strlen($username) < $minimun ) {
-  
-      echo "Username has to be longer than five";
-  
-  }  
-    
-    if(strlen($username) > $maximun  ) {
-  
-      echo "Username cannot be longer than 30 ";
-  
-  }  
-    
-
-
-}
-
-
-
-?>
 <html>
 <head>
 	<title>PHP Starter Application</title>
@@ -42,7 +11,7 @@ $password = $_POST['password'];
 		<h1>Welcome</h1>
 		
 	
-		<form action="index.php" method="post">
+		<form action="process_form.php" method="post">
     
 <input type="text" name="username" placeholder="Enter Username">
 <input type="password"  name="password" placeholder="Enter Password"><br>
