c446c61eed7788462a8e31f694cacb27ca976806
wu <wu673@purdue.edu>
2016-04-13 22:21:09 -0400

    add bootstrap

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>PHP Starter Application</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
 <div class="wrapper">
