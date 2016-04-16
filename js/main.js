15c55e08a14c6249ed849e6fa07d6acaf4241dae
maoxia <maoxia.purdue.edu>
2016-04-15 23:53:17 -0400

    update most recent html

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+     
+});
+function validateForm(){
+    var firstName = $("#firstname").val(),
+        lastName = $("#lastname").val(),
+        email1 = $("#email1").val(),
+        email2 = $("#email2").val(),
+        newPassword = $("#new-password").val(),
+        month = $("#birth-month").val(),
+        day = $("#birth-day").val(),
+        year = $("#birth-year").val(),
+        female = $("#female").val(),
+        male = $("#male").val();
+}
+
